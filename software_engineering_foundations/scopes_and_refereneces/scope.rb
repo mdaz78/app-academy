7c3aad78eb6a943e626d6b729184ed355233dfa7
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-10-07 20:11:36 +0530

    Scope notes

---
+++ software_engineering_foundations/scopes_and_refereneces/scope.rb
@@ -0,0 +1,30 @@
+## scope example
+# hello = "zafar"
+
+# def say_hello
+#   p hello
+# end
+
+# p hello
+# say_hello
+
+
+## Global varibales example
+# $message = "hello Global"
+
+# def say_hello
+#   p $message
+# end
+
+# p $message
+# say_hello
+
+
+## Constant example
+# MESSAGE = "hello"
+# def print_message
+#   p MESSAGE
+# end
+
+# p MESSAGE
+# print_message
