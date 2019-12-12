5f4c0aa73894e4c4def440ae00509fdbc8f3d01a
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-12-12 12:06:07 +0530

    Solve part 1 of the Hotel Project

--- software_engineering_foundations/input_output/hotel_project/lib/room.rb
+++ software_engineering_foundations/input_output/hotel_project/lib/room.rb
@@ -1,3 +1,35 @@
 class Room
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
 
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def total_occupants
+    @occupants.length
+  end
+
+  def full?
+    @capacity == self.total_occupants
+  end
+
+  def available_space
+    @capacity - self.total_occupants
+  end
+
+  def add_occupant(name)
+    if self.full?
+      false
+    else
+      @occupants << name
+      true
+    end
+  end
 end
