e3932ccbc1eade9ee396a293d5c507455ca6176b
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-12-12 12:31:23 +0530

    Solve part 2 of the hotel project

--- software_engineering_foundations/input_output/hotel_project/lib/hotel.rb
+++ software_engineering_foundations/input_output/hotel_project/lib/hotel.rb
@@ -1,5 +1,48 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(name, room_hash)
+    @name = name
+    @rooms = Hash.new()
+    room_hash.map do |room_name, room_capacity|
+      @rooms[room_name] = Room.new(room_capacity)
+    end
+  end
+
+  def name
+    @name.split(' ').map { |word| word.capitalize }.join(' ')
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists?(room_name)
+    room_names = @rooms.keys
+    room_names.include?(room_name)
+  end
+
+  def check_in(person, room_name)
+    if self.room_exists?(room_name)
+      if @rooms[room_name].add_occupant(room_name)
+        p "check in successful"
+      else
+        p "sorry, room is full"
+      end
+    else
+      p "sorry, room does not exist"
+    end
+  end
+
+  def has_vacancy?
+    @rooms.values.any? do |room|
+      !room.full?
+    end
+  end
+
+  def list_rooms
+    @rooms.each do |name, room|
+      puts "#{name} : #{room.available_space}"
+    end
+  end
 end
