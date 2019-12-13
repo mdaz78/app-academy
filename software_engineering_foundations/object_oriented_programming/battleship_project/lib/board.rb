856c7f7916187fa8c4eb938de1a03dff6dc9afb3
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-12-13 10:50:28 +0530

    Solve part 1 of the battleship project

--- software_engineering_foundations/object_oriented_programming/battleship_project/lib/board.rb
+++ software_engineering_foundations/object_oriented_programming/battleship_project/lib/board.rb
@@ -1,3 +1,25 @@
 class Board
-  
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) { Array.new(n, :N) }
+    @size = n * n
+  end
+
+  def [](pos)
+    row, column = pos
+    @grid[row][column]
+  end
+
+  def []=(pos, value)
+    row, column = pos
+    @grid[row][column] = value
+  end
+
+  def num_ships
+    total_num_of_ships = @grid.inject(0) do |acc, arr|
+      ships_per_row = arr.count { |el| el == :S }
+      acc + ships_per_row
+    end
+  end
 end
