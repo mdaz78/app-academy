ec661ad10e5ad7cc51afb6246bd5a4c20db87fb3
Md Abu Zafar <mdabuzafar78@gmail.com>
2019-12-13 13:06:29 +0530

    Solve part 3 of the battleship project

--- software_engineering_foundations/object_oriented_programming/battleship_project/lib/battleship.rb
+++ software_engineering_foundations/object_oriented_programming/battleship_project/lib/battleship.rb
@@ -2,5 +2,48 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_reader :board, :player
 
+  def initialize(length_of_board)
+    @player = Player.new()
+    @board = Board.new(length_of_board)
+    @remaining_misses = @board.size / 2
+  end
+
+  def start_game
+    @board.place_random_ships
+    puts "Number of ships => #{@board.num_ships}"
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      puts "you lose"
+      true
+    else
+      false
+    end
+  end
+
+  def win?
+    if @board.num_ships == 0
+      puts "you win"
+      true
+    else
+      false
+    end
+  end
+
+  def game_over?
+    win? || lose?
+  end
+
+  def turn
+    position = @player.get_move
+    @board.print
+    unless @board.attack(position)
+      @remaining_misses -= 1
+    end
+    puts @remaining_misses
+  end
 end
