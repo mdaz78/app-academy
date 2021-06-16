6151c63d9eabbdaff8c3c38d0f36c1c5bd7ffe92
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-06-16 20:07:14 +0530

    Solve dog project

--- revision/May/classes/dog_project/lib/dog.rb
+++ revision/May/classes/dog_project/lib/dog.rb
@@ -1,3 +1,39 @@
 class Dog
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
 
+  def bark
+    return "BORK!" if @age >= 5
+    "bork!"
+  end
+
+  def favorite_food?(food)
+    serialized_favorited_foods = @favorite_foods.map { |favorite_food| favorite_food.downcase }
+    serialized_favorited_foods.include? (food.downcase)
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def age
+    @age
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  def favorite_foods
+    @favorite_foods
+  end
 end
